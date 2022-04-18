ecaad9ff3bb17707096ff9fcb7713817f091ec93
cmeggs0 <cmeggs0@chicagobooth.edu>
2022-04-18 20:48:28 +0000

    added some accessors

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :photos
+  has_many :likes
+  has_many :follow_requests
+  has_many :senders, through: :follow_requests
 end
