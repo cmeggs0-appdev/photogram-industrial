c570c3ea3dd5cba602ce382f8cf77948a613b784
cmeggs0 <cmeggs0@chicagobooth.edu>
2022-04-19 01:18:34 +0000

    migration file changes

--- db/migrate/20220419011511_add_phots_count_to_users.rb
+++ db/migrate/20220419011511_add_phots_count_to_users.rb
@@ -1,5 +1,5 @@
 class AddPhotsCountToUsers < ActiveRecord::Migration[6.1]
   def change
-      add_column :users, :photo_count, :integer
+      add_column :users, :photo_count, :integerg
   end
 end
